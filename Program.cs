5388424851d6dad4ce9d235538752526d35cc583
jazmy <jazmy@JAZ-LAPTOP>
2025-05-13 15:03:31 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿Dictionary<int, int> countNumbers = new Dictionary<int, int>();
+int[] myArray = { 4, 1, 2, 2, 1 };   
+
+foreach (int num in myArray)
+
+{
+    if (countNumbers.ContainsKey(num))
+    {
+        countNumbers[num]++;
+    }
+    else
+    {
+        countNumbers[num] = 1;
+
+    }
+}
+
+foreach (var pair in countNumbers)
+{
+    if (pair.Value == 1)
+    {
+        Console.Write(pair.Key);
+    }
+}
